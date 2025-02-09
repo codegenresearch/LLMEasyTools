1b493d1a6f2e63bf49bf7495df4b952c5f7bb3ac
venkatkrishna <venkatkrishna@moe>
2025-02-09 16:54:18 +0100

    Reset to 25185ad68f7d2156de6651980f6a355d95cce4f4 and made changes for LLM Round 1

--- tests/processor_test.py
+++ tests/processor_test.py
@@ -13,9 +13,6 @@ def mk_tool_call(name, args):
     arguments = json.dumps(args)
     return SimpleToolCall(id='A', function=SimpleFunction(name=name, arguments=arguments), type='function')
 
-def mk_tool_call_jason(name, args):
-    return SimpleToolCall(id='A', function=SimpleFunction(name=name, arguments=args), type='function')
-
 def mk_chat_completion(tool_calls):
     return SimpleCompletion(
         id='A',
