1258cc97945f93bc026144bde27e03ed065a1c2e
venkatkrishna <venkatkrishna@moe>
2025-02-09 08:59:06 +0100

    Reset to 25185ad68f7d2156de6651980f6a355d95cce4f4 and made changes for LLM Round 1

--- llm_easy_tools/__init__.py
+++ llm_easy_tools/__init__.py
@@ -1,1 +1,1 @@
-from .schema_generator import generate_function_schema, add_prefix, extract_tool_definitions, LLMFunction from .processor import handle_response, handle_message, execute_tool_call, ToolExecutionResult
+from .schema_generator import generate_function_schema, add_prefix, extract_tool_definitions, LLMFunction from .processor import handle_response, handle_message, execute_tool_call, ToolResult
